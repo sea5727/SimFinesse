b5f7b870d5b340e7b1a0a19d1f6d95a58edd0116
ysh8361 <ysh8361@localhost.localdomain>
2020-01-02 18:19:08 +0900

    commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+var commander = require('./utils/commander')
+
+console.log(`your http port : ${commander.httpPort}, xmpp port : ${commander.xmppPort}`)
+
 let http_server = require('./http_server')
 let xmpp_server = require('./xmpp_server')
 
-http_server(3000)
-xmpp_server(5222)
+http_server(commander.httpPort)
+xmpp_server(commander.xmppPort)
